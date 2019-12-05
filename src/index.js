755ac3d1b4efbcfdfb688a3361a404dfff59c992
rstix <rsonroad@gmail.com>
2019-12-05 13:26:54 +0100

    provider added

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux"; // it is a component that is provided by the react-redux, it will sitt on top of all the components
+import { createStore } from "redux"; // function from redux, it gets all the reducers and returns all the data (state) and redux reducers
+
 import App from "./components/App";
+import reducers from "./reducers";
 
 import "./styles.css";
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+  <Provider store={createStore(reducers)}>
+    <App />
+  </Provider>,
+  rootElement
+);
