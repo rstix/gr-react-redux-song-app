1ad8dd434aa13228c67dc0f61c32ab4a202de49b
rstix <rsonroad@gmail.com>
2019-12-06 08:21:51 +0100

    props passed from redux to react and rendered

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,19 +1,29 @@
-import React from 'react'
+import React from "react";
 
-import { connect } from 'react-redux'
+import { connect } from "react-redux";
 
 class SongList extends React.Component {
-  render(){
-    return (
-      <div> 
-      SongList 
-      </div>
-    )
+  renderList() {
+    return this.props.songs.map(song => {
+      return (
+        <div className="item" key={song.title}>
+          <div className="right floated content">
+            <button className="ui button primary">Select</button>
+          </div>
+
+          <div className="content">{song.title}</div>
+        </div>
+      );
+    });
+  }
+
+  render() {
+    return <div className="ui divided list">{this.renderList()}</div>;
   }
 }
 
-const mapStateToProps = (state) => {
-  return { songs: state.songs }
-}
+const mapStateToProps = state => {
+  return { songs: state.songs };
+};
 
-export default connect(mapStateToProps)(SongList) // connect creates a react component
+export default connect(mapStateToProps)(SongList); // connect creates a react component
